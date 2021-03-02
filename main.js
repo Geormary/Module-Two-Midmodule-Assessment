59c8a551d6da7839d03146fd69c0d4d453a04054
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-03-02 12:41:30 -0500

    set goal and read up on documentation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//My SMART GOAL is: track the button click event and check for it through console.log 
+//My STRETCH Goals: Create a conditional statement and paste my code in it so that it adds to history on second click 
 let values = { "⚀": 1, "⚁": 2, "⚂": 3, "⚃": 4, "⚄": 5, "⚅": 6 };
 let values1 = { 1: "⚀", 2: "⚁", 3: "⚂", 4: "⚃", 5: "⚄", 6: "⚅" };
 let die = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
@@ -7,26 +9,29 @@ document.addEventListener("DOMContentLoaded", () => {
   let dicePara = document.getElementById("dice-para");
   let button = document.getElementById("dice-roll");
   let sumPara = document.getElementById("sum-para");
-  let historyList = document.getElementById("history-list")
+  let historyList = document.getElementById("history-list");
   button.addEventListener("click", (e) => {
+    let count = 0;
+    if ("click") {
+      count++;
+      console.log(`this is the count: ${count}`)
+    }
     let diceStr = "";
     let sum = 0;
     for (let i = 0; i < inputNum.value; i++) {
-      let randNum = Math.floor(Math.random() * (die.length));
+      let randNum = Math.floor(Math.random() * die.length);
       diceStr += die[randNum];
-      console.log(randNum)
-      sum += (randNum+1)
+      console.log(randNum);
+      sum += Number(randNum + 1);
     }
     dicePara.textContent = `Dice: ${diceStr}`;
     sumPara.textContent = `Sum = ${sum}`;
-    
-    let li = document.createElement("li")
-    li.textContent = `Roll: ${diceStr} Sum: ${sum}`
-    historyList.appendChild(li)
-    console.log(historyList.textContent) 
+
+    let li = document.createElement("li");
+    li.textContent = `Roll: ${diceStr} Sum: ${sum}`;
+    historyList.appendChild(li);
+    console.log(historyList.textContent);
   });
-
-
 });
 
 //when click button
